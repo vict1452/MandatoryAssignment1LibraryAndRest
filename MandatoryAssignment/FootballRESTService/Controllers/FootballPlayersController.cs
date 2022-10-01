7461110b5fc86c58102faa3ded1e303c49b95829
MitzDK <victorwilliamjejlskov@gmail.com>
2022-10-01 15:00:00 +0200

    1

---
+++ MandatoryAssignment/FootballRESTService/Controllers/FootballPlayersController.cs
@@ -0,0 +1,78 @@
+﻿using FootballRESTService.Managers;
+using MandatoryAssignment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballRESTService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FootballPlayersController : Controller
+    {
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<FootballPlayer>> Get()
+        {
+            var list = _manager.GetAll();
+            if (list == null) return NotFound("No such list was found");
+            if (list.Count == 0) return NoContent();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<FootballPlayer> Get(int id)
+        {
+            FootballPlayer player = _manager.GetById(id);
+            if (player == null) return NotFound("No such player: id: " + id);
+            return player;
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public ActionResult<FootballPlayer> Post([FromBody] FootballPlayer value)
+        {
+            try
+            {
+                value.Validate();
+            }
+            catch
+            {
+                return BadRequest("The given data is invalid");
+            }
+            if (_manager.GetAll().Exists(player => player.ShirtNumber == value.ShirtNumber)) return Conflict("A player with this shirtnumber already exists");
+            _manager.Add(value);
+            return Created(value.Id.ToString(), value);
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public ActionResult<FootballPlayer> Put(int id, [FromBody] FootballPlayer value)
+        {
+            try
+            {
+                value.Validate();
+            }
+            catch
+            {
+                return BadRequest("The given data is invalid");
+            }
+            if (_manager.GetById(id).ShirtNumber != value.ShirtNumber && _manager.GetAll().Exists(player => player.ShirtNumber == value.ShirtNumber)) return Conflict("A different player with this shirtnumber already exists");
+            return Ok(_manager.Update(id, value));
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<FootballPlayer> Delete(int id)
+        {
+            if (_manager.GetById(id) == null) return NotFound($"No player with id: {id} was found");
+            return Ok(_manager.Delete(id));
+        }
+    }
+}
---
+++ MandatoryAssignment/FootballRESTService/Managers/FootballPlayersManager.cs
@@ -0,0 +1,47 @@
+﻿using MandatoryAssignment;
+
+namespace FootballRESTService.Managers
+{
+    public class FootballPlayersManager
+    {
+        private static int _nextId = 1;
+        private static List<FootballPlayer> _players = new List<FootballPlayer>()
+        {
+            new FootballPlayer(){Id = _nextId++, Name = "Victor", Age = 25, ShirtNumber = 1 },
+            new FootballPlayer(){Id = _nextId++, Name = "Martin", Age = 29, ShirtNumber = 11 },
+            new FootballPlayer(){Id = _nextId++, Name = "Andreas", Age = 26, ShirtNumber = 8 },
+            new FootballPlayer(){Id = _nextId++, Name = "Frederik", Age = 24, ShirtNumber = 4 }
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return _players;
+        }
+        public FootballPlayer GetById(int id)
+        {
+            return _players.Find(player => player.Id == id);
+        }
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.Id = _nextId++;
+            _players.Add(newPlayer);
+            return newPlayer;
+        }
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer player = GetById(id);
+            if (player == null) return null;
+            _players.Remove(player);
+            return player;
+        }
+        public FootballPlayer Update(int id, FootballPlayer update)
+        {
+            FootballPlayer player = GetById(id);
+            if (player == null) return null;
+            player.Name = update.Name;
+            player.ShirtNumber = update.ShirtNumber;
+            player.Age = update.Age;
+            return player;
+        }
+    }
+}
